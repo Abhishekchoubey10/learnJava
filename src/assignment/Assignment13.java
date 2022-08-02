e94359a9c1c5ec59ef1fdc9abcf14630bb3231fe
Abhishekchoubey10 <abhishek10mar@gmail.com>
2022-08-02 20:25:43 +0530

    added code in assignment13

--- src/assignment/Assignment13.java
+++ src/assignment/Assignment13.java
@@ -4,8 +4,18 @@ public class Assignment13 {
 
 	public static void main(String[] args) {
 		
-		
+		/**
+		 * @author Abhishek kumar
+		 * Print 1 to 10 and break the loop once you find multiplication of 7 with a message "bye see you tomorrow"
+		 */
 
+		for(int i =1; i<=10; i++) {
+			System.out.println(i);
+			if(i % 7 == 0) {
+				System.out.println("bye See you tomorrow");
+				break;
+			}
+		}
 	}
 
 }
