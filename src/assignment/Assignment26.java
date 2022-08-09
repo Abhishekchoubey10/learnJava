505f348cfe8fec98a59c512f24407f9fd6fe7c4c
Abhishekchoubey10 <abhishek10mar@gmail.com>
2022-08-09 20:24:25 +0530

    Adding some more java Assignment code

---
+++ src/assignment/Assignment26.java
@@ -0,0 +1,27 @@
+package assignment;
+
+import java.util.ArrayList;
+
+public class Assignment26 {
+
+	public static void main(String[] args) {
+		/**
+		 * Write a java program to print all the elements of a ArrayList using the position of the elements
+		 * 
+		 * @author Abhishek kumar
+		 */
+		ArrayList<String> ay = new ArrayList<String>();
+		ay.add("298");
+		ay.add("Prakash");
+		ay.add("Abhishek");
+		ay.add("Ram");
+		ay.add("Uttam");
+		
+		for(int i =0; i< ay.size(); i++) {
+			System.out.println(ay.get(i));
+		}
+		
+
+	}
+
+}
