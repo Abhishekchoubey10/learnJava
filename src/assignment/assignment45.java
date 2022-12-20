f80c6aaf14442c239b0663dc8e69067f58807311
Abhishekchoubey10 <abhishek10mar@gmail.com>
2022-12-20 15:54:32 +0530

    added new assignment

---
+++ src/assignment/assignment45.java
@@ -0,0 +1,11 @@
+package assignment;
+
+public class assignment45 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+	}
+
+}
